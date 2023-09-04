e851f06a5607eac011c84d4948850ac6ec33a5e1
owenowenisme <mses010108@gmail.com>
2023-09-04 13:35:13 +0800

    feat: update 404 page

--- react-pastexam/src/pages/NoPage/nopage.jsx
+++ react-pastexam/src/pages/NoPage/nopage.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { Sidebar } from "../../conponents/Sidebar/sidebar";
+import { Navbar } from "../../conponents/Navbar/navbar";
+import { Box } from "@mui/system";
+import {Footer} from "../../conponents/Footer/footer";
+import { Typography } from "@mui/material";
 export const NoPage = () => {
-    return <h1>Oops! 這個頁面不存在</h1>;
+    return (
+      <Box sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column' ,backgroundColor:'#040D12'}}>
+      <Box sx={{ display: 'flex' ,backgroundColor:'#040D12'}} >
+          <Navbar />
+          <Sidebar />
+          <Typography variant="h1" component="div" color={"white"} sx={{ flexGrow: 1 ,paddingTop:'75px'}}>
+            Oops! Page not found.
+          </Typography>
+      </Box>
+      <Footer />
+  </Box>
+    )
   };
