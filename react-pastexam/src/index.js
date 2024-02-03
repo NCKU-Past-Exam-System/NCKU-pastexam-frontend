75036b5c283eb4396311af25717f18103f836d08
owenowenisme <mses010108@gmail.com>
2024-02-04 01:23:39 +0800

    style: remove unused import

--- react-pastexam/src/index.js
+++ react-pastexam/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home/home';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Main } from './pages/Main/main';
-import { Login } from './pages/Login/login';
-import { NoPage } from './pages/NoPage/nopage';
-import { Upload } from './pages/Upload/upload';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import reportWebVitals from './reportWebVitals';
+import { Main } from './pages/Main/main';
+import { NoPage } from './pages/NoPage/nopage';
 import { clientId } from './credential';
 import { Search } from './pages/Search/search';
 import { Tmp } from './pages/Tmp/tmp';
 import { Files } from './pages/Files/files';
 import { MyFiles } from './pages/MyFiles/myFiles';
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -20,28 +18,28 @@ export default function App() {
         {/* <Route path="*" element={<Maintainance />}/> */}
         <Route path="/" element={<Main />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/files/:id" element={<Files/>} />
+        <Route path="/files/:id" element={<Files />} />
         <Route path="/search" element={<Search />} />
         <Route path="/tmp" element={<Tmp />} />
         <Route path="/myfiles" element={<MyFiles />} />
-        <Route path="*" element={<NoPage />}/>
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-{/* <Route path="*" element={<NoPage />}/>
+{ /* <Route path="*" element={<NoPage />}/>
 <Route path="/" element={<Main />} />
 <Route path="/main" element={<Main />} />
 <Route path="/main/:id" element={<Main/>} />
 <Route path="/login" element={<Login />} />
-<Route path="/upload" element={<Upload />} /> */}
+<Route path="/upload" element={<Upload />} /> */ }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-document.body.style.backgroundColor = "#080808";
+document.body.style.backgroundColor = '#080808';
 root.render(
   // <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId} >
+  <GoogleOAuthProvider clientId={clientId}>
     <App />
-    </GoogleOAuthProvider>
+  </GoogleOAuthProvider>,
   // </React.StrictMode>
 );
 
