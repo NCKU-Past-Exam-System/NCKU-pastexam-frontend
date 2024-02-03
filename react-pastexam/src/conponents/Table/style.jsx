bfc12461a83028b550d7f6946c25642851416c4c
owenowenisme <mses010108@gmail.com>
2024-02-04 01:20:01 +0800

    chore: delete unnecessary components

--- react-pastexam/src/conponents/Table/style.jsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as LoadingComponent } from '../../conponents/Svg/loading.svg';
-import { styled } from 'styled-components';
-export const Loading = styled(LoadingComponent)`
-  display: block;
-  margin: 20px auto;
-  width: 50px;
-  fill: #fff;
-
-`;     
